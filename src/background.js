ab62059bb81737ca27fc99c9b51ad958922300ad
Wellyngton Silva <wellyngton61@gmail.com>
2023-07-08 22:27:47 -0300

    Commit inicial extensão

---
+++ src/background.js
@@ -0,0 +1,42 @@
+// Function to reduce volume in all tabs
+function reduceTabVolumes(tabId) {
+    chrome.tabs.query({}, function (tabs) {
+      tabs.forEach(function (tab) {
+        if(tabId !== tab.id) {
+            chrome.tabs.executeScript(tab.id, {
+              code: "Array.from(document.getElementsByTagName('video')).forEach(function(video) { video.volume = 0.1; });"
+            });
+        }
+      });
+    });
+    chrome.tabs.executeScript(tabId, {
+        code: "Array.from(document.getElementsByTagName('video')).forEach(function(video) { video.volume = 1; });"
+      });
+}
+
+function restoreTabVolumes() {
+    chrome.tabs.query({}, function (tabs) {
+      tabs.forEach(function (tab) {
+            chrome.tabs.executeScript(tab.id, {
+              code: "Array.from(document.getElementsByTagName('video')).forEach(function(video) { video.volume = 1; });"
+            });
+      });
+    });
+}
+  
+  
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, _) {
+    if (changeInfo.audible) {
+        reduceTabVolumes(tabId)
+    } else if (!changeInfo.audible) {
+        restoreTabVolumes()
+    }
+});
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function (tab) {
+        if (tab.audible) {
+            reduceTabVolumes(activeInfo.tabId);
+        }
+    });
+});
